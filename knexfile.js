4e6279e1d4deeacfa23dadab4076387df9b0ef27
Robert <rc3331@gmail.com>
2019-11-17 18:33:57 -0600

    committing migration data for knex

--- knexfile.js
+++ knexfile.js
@@ -35,6 +35,9 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
     pool: {
       min: 2,
       max: 10
