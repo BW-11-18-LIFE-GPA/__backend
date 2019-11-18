724920be1a540dcac7715ec22b78b0db4649f2f1
Robert <rc3331@gmail.com>
2019-11-17 18:38:14 -0600

    another bugfix for migrations

--- knexfile.js
+++ knexfile.js
@@ -35,15 +35,13 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './data/migrations'
-    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
     }
   }
 
